eea8b50a4e493eac4d7d419cb31d69794dbb3224
biaxiao <953692809@qq.com>
2019-05-28 19:42:03 +0800

    整合MongoDB，实现用户浏览记录在MongoDB中添加、删除、查询等

---
+++ src/main/java/com/x/mall/nosql/mongodb/ducument/MemberReadHistory.java
@@ -0,0 +1,117 @@
+package com.x.mall.nosql.mongodb.ducument;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * 用户浏览历史记录
+ */
+@Document  //表示映射到MongoDB文档上的领域对象
+public class MemberReadHistory {
+
+    @Id  //表示某个域为ID域
+    private String id;
+
+    @Indexed  //表示某个字段为MongoDB的索引字段
+    private Long memberId;
+
+    private String memberNickname;
+
+    private String memberIcon;
+
+    @Indexed
+    private Long productId;
+
+    private String productName;
+
+    private String productPic;
+
+    private String productSubTitle;
+
+    private String productPrice;
+
+    private Date createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberNickname() {
+        return memberNickname;
+    }
+
+    public void setMemberNickname(String memberNickname) {
+        this.memberNickname = memberNickname;
+    }
+
+    public String getMemberIcon() {
+        return memberIcon;
+    }
+
+    public void setMemberIcon(String memberIcon) {
+        this.memberIcon = memberIcon;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPic() {
+        return productPic;
+    }
+
+    public void setProductPic(String productPic) {
+        this.productPic = productPic;
+    }
+
+    public String getProductSubTitle() {
+        return productSubTitle;
+    }
+
+    public void setProductSubTitle(String productSubTitle) {
+        this.productSubTitle = productSubTitle;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
